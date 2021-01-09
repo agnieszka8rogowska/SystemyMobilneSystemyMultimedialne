c17efd875eb12db273fb01b9240a739f92da36cf
Agnieszka Rogowska <62265152+agnieszka8rogowska@users.noreply.github.com>
2021-01-09 19:53:27 +0100

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,17 +1,17 @@
 let Peer = require('simple-peer')
 let socket = io()
 const video = document.querySelector('video')
-const filter = document.querySelector('#filter')
-const checkboxTheme = document.querySelector('#theme')
+const filter = document.querySelector('#filter')// filtr
+const checkboxTheme = document.querySelector('#theme')//motyw
 let client = {}
 let currentFilter
-//get stream
+
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
-        socket.emit('NewClient')
-        video.srcObject = stream
+        socket.emit('NewClient') //dodanie nowego klienta
+        video.srcObject = stream //wyświetlanie własnego wideo
         video.play()
-
+        //sprawdzanie czy wybrano filtr dla wideo, jeśli tak to włączenie wybranego filtru 
         filter.addEventListener('change', (event) => {
             currentFilter = event.target.value
             video.style.filter = currentFilter
@@ -19,18 +19,17 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             event.preventDefault
         })
 
-        //used to initialize a peer
-        function InitPeer(type) {
+        function InitPeer(type) { //funkcja definiująca nowego użytkowinka i zwracająca go
             let peer = new Peer({ initiator: (type == 'init') ? true : false, stream: stream, trickle: false })
-            peer.on('stream', function (stream) {
-                CreateVideo(stream)
+            peer.on('stream', function (stream) { //pobranie strumienia od innego uzytkownika
+                CreateVideo(stream) //tworzenie wideo drugiego użytkownika (funkcja napisana niżej)
             })
-            //This isn't working in chrome; works perfectly in firefox.
-            // peer.on('close', function () {
-            //     document.getElementById("peerVideo").remove();
-            //     peer.destroy()
-            // })
-            peer.on('data', function (data) {
+            //zakomentować jeśli używamy chrome; działa na firefox.
+            /*peer.on('close', function () { //usuwanie wideo
+            document.getElementById("peerVideo").remove();
+            peer.destroy()
+            })*/
+            peer.on('data', function (data) { //przejazanie wybranego filtru 
                 let decodedData = new TextDecoder('utf-8').decode(data)
                 let peervideo = document.querySelector('#peerVideo')
                 peervideo.style.filter = decodedData
@@ -38,8 +37,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             return peer
         }
 
-        //for peer of type init
-        function MakePeer() {
+        function MakePeer() { //tworzenie użytkownika
             client.gotAnswer = false
             let peer = InitPeer('init')
             peer.on('signal', function (data) {
@@ -50,8 +48,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             client.peer = peer
         }
 
-        //for peer of type not init
-        function FrontAnswer(offer) {
+        function FrontAnswer(offer) { //dla użytkownika !init
             let peer = InitPeer('notInit')
             peer.on('signal', (data) => {
                 socket.emit('Answer', data)
@@ -60,38 +57,35 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             client.peer = peer
         }
 
-        function SignalAnswer(answer) {
+        function SignalAnswer(answer) {//funkcja używana, gdy przyjdzie odpowiedz (answer)
             client.gotAnswer = true
             let peer = client.peer
             peer.signal(answer)
         }
 
-        function CreateVideo(stream) {
-            CreateDiv()
-
+        function CreateVideo(stream) {//funkcja tworząca wideo
+            CreateDiv() //funkcja utworzona niżej w kodzie
             let video = document.createElement('video')
             video.id = 'peerVideo'
             video.srcObject = stream
             video.setAttribute('class', 'embed-responsive-item')
             document.querySelector('#peerDiv').appendChild(video)
             video.play()
-            //wait for 1 sec
-            setTimeout(() => SendFilter(currentFilter), 1000)
-
+            SendFilter(currentFilter)
             video.addEventListener('click', () => {
                 if (video.volume != 0)
                     video.volume = 0
                 else
                     video.volume = 1
             })
-
+        }
+        
+        function SessionActive() { //funkcja wyświetla komunikat, jeśli już jest dwóch użytkowników aktywnych i będzie próbował podłączyć się kolejny
+            alert('Aktualnie trwa aktywna sesja. Video-chat przeznaczony jest dla 2 osób. Spróbuj ponownie póżniej!')
+            document.write('<h5>Odśwież stronę</h5>');
         }
 
-        function SessionActive() {
-            document.write('Session Active. Please come back later')
-        }
-
-        function SendFilter(filter) {
+        function SendFilter(filter) { //funkcja wysyła filtr
             if (client.peer) {
                 client.peer.send(filter)
             }
@@ -105,12 +99,12 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             }
         }
 
+        //wywołanie funkcji dla konkretnych zdarzeń:
         socket.on('BackOffer', FrontAnswer)
         socket.on('BackAnswer', SignalAnswer)
         socket.on('SessionActive', SessionActive)
         socket.on('CreatePeer', MakePeer)
         socket.on('Disconnect', RemovePeer)
-
     })
     .catch(err => document.write(err))
 
@@ -120,7 +114,6 @@ checkboxTheme.addEventListener('click', () => {
         if (document.querySelector('#muteText')) {
             document.querySelector('#muteText').style.color = "#fff"
         }
-
     }
     else {
         document.body.style.backgroundColor = '#fff'
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
+//dołączenie potrzebnych zależnośći
+
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public")) //dzięki temu aplikacja będzie wiedziała, gdzie znajduje się index.html
 let clients = 0
 
-io.on('connection', function (socket) {
+io.on('connection', function (socket) { //działanie aplikacji, jeśli istanieje połączenie
     socket.on("NewClient", function () {
         if (clients < 2) {
             if (clients == 1) {
                 this.emit('CreatePeer')
             }
         }
-        else
+        else //jeśli jest więcej niż 2 klientów to wyświetli komunikat
             this.emit('SessionActive')
         clients++;
     })
+    //wywołanie funkcji dla konkretnych zdarzeń:
     socket.on('Offer', SendOffer)
     socket.on('Answer', SendAnswer)
     socket.on('disconnect', Disconnect)
@@ -38,7 +41,7 @@ function SendOffer(offer) {
 function SendAnswer(data) {
     this.broadcast.emit("BackAnswer", data)
 }
-
+//nasłuhiwanie serwera na podanym porcie
 http.listen(port, () => console.log(`Active on ${port} port`))
 
 
